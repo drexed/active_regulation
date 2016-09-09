0a5f7d6735988276397829e28f4bcc1df7020dba
Juan Gomez <j.gomez@drexed.com>
2016-09-08 23:01:42 -0400

    add specs back

--- lib/active_regulation.rb
+++ lib/active_regulation.rb
@@ -1,3 +1,3 @@
 %w(version activation containment expiration quarantine suspension visibility).each do |file_name|
-  require "regulation/#{file_name}"
+  require "active_regulation/#{file_name}"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,30 @@
-require "coveralls"
+require 'coveralls'
 Coveralls.wear!
 
-require "active_record"
-require "active_support"
-require "active_regulation"
-require "pathname"
-require "database_cleaner"
+require 'active_record'
+require 'active_support'
+require 'active_regulation'
+require 'pathname'
+require 'database_cleaner'
 
 module Rails
   def self.env
-    "test"
+    'test'
   end
 end
 
-I18n.load_path << File.expand_path("../../config/locales/en.yml", __FILE__)
+spec_support_path = Pathname.new(File.expand_path('../spec/support', File.dirname(__FILE__)))
+
+I18n.load_path << File.expand_path('../../config/locales/en.yml', __FILE__)
 I18n.enforce_available_locales = false
 
-spec_support_path = Pathname.new(File.expand_path("../spec/support", File.dirname(__FILE__)))
-
-ActiveRecord::Base.configurations = YAML::load_file(spec_support_path.join("config/database.yml"))
+ActiveRecord::Base.configurations = YAML::load_file(spec_support_path.join('config/database.yml'))
 ActiveRecord::Base.establish_connection
 
-load(spec_support_path.join("db/schema.rb")) if File.exist?(spec_support_path.join("db/schema.rb"))
+load(spec_support_path.join('db/schema.rb')) if File.exist?(spec_support_path.join('db/schema.rb'))
 
-Dir.glob(spec_support_path.join("models/*.rb"))
-   .each { |f| autoload(File.basename(f).chomp(".rb").camelcase.intern, f) }
+Dir.glob(spec_support_path.join('models/*.rb'))
+   .each { |f| autoload(File.basename(f).chomp('.rb').camelcase.intern, f) }
    .each { |f| require(f) }
 
 RSpec.configure do |config|
